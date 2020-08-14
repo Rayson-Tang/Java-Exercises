1108e498d28cedcff586a3ac8f381befddf97f78
tmr620 <535704@qq.com>
2020-08-14 10:16:06 +0800

    Java

---
+++ src/com/Rayson/leetcode/Solution1.java
@@ -0,0 +1,21 @@
+package com.Rayson.leetcode;
+
+import java.util.HashMap;
+
+public class Solution1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = target - nums[i];
+            if (map.containsKey(num)) {
+                return new int[]{map.get(num), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
