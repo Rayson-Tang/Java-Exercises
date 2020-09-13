d1f143efa2bd27ca5218bd0ce652f52dcc7605c2
tmr620 <535704@qq.com>
2020-09-13 11:13:34 +0800

    java

---
+++ src/com/Rayson/leetcode/text.java
@@ -0,0 +1,17 @@
+package com.Rayson.leetcode;
+
+public class text {
+    public static void main(String[] args) {
+//        int[][] ints = new int[4][];
+//        for (int i = 0; i < ints.length; i++) {
+//            for (int j = 0; j < ints[0].length; j++) {
+//                ints[i][j]=1;
+//            }
+//        }
+//        System.out.println(ints);
+        int a[][] = new int[5][];
+        a[0]=new  int[3];
+        a[0][0]=1;
+        System.out.println(a[0][0]);
+    }
+}
