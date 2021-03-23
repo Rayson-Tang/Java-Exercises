e56522eb7ef35702beb6bd5bddda02980d1d6519
tmr620 <535704@qq.com>
2021-03-23 18:22:54 +0800

    java

--- src/com/Rayson/dataStructures/linkedList/CircleLinkedList.java
+++ src/com/Rayson/dataStructures/linkedList/CircleLinkedList.java
@@ -5,8 +5,45 @@ public class CircleLinkedList {
         CircleLinked circleLinked = new CircleLinked();
         circleLinked.add(5);
         circleLinked.show();
+        System.out.println("出列");
+        countBoy(1,5,2,circleLinked.getFirst());
     }
 
+    public static void countBoy(int start, int total, int count, BoyNode first){
+        if (start <= 0 || total <= 0 || count <= 0 || start >= total){
+            System.out.println("输入错误");
+            return;
+        }
+        CircleLinked circleLinked = new CircleLinked();
+        BoyNode helper = first;
+        //helper指向最后节点
+        while (true){
+            if (helper.getNext() == first){
+                break;
+            }
+            helper = helper.getNext();
+        }
+        //first指向开始节点，helper指向开始前一个
+        for (int i = 0; i < start-1; i++) {
+            first = first.getNext();
+            helper = helper.getNext();
+        }
+        while (true) {
+            //开始数数
+            for (int i = 0; i < count - 1; i++) {
+                first = first.getNext();
+                helper = helper.getNext();
+            }
+            //出列
+            System.out.println(first.no);
+            first = first.getNext();
+            helper.setNext(first);
+            if (helper == first){
+                break;
+            }
+        }
+        System.out.println(first.no);
+    }
 
     static class CircleLinked{
         private BoyNode first =new BoyNode(-1);
